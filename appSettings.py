8393eeef8cf4f919ed2d864f346022972ec4e1d8
wasi300 <horiguchiko@gmail.com>
2014-10-31 16:21:50 +0900

    abesi

--- appSettings.py
+++ appSettings.py
@@ -10,8 +10,3 @@ class setting :
 		self.k.append('----------------------------------------------------')
 		self.k.append('--------------------------------------------')
 
-#url = "https://api.twitter.com/1.1/users/show.json"
-#urlSearchTweet = "https://api.twitter.com/1.1/search/tweets.json"
-#urlFriendTwitter = "https://api.twitter.com/1.1/friendships/create.json"
-#url = "https://api.twitter.com/1.1/users/suggestions/:slug.json"
-#url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
--- bridge.py
+++ bridge.py
@@ -2,6 +2,12 @@
 
 class bridge:
 
+#url = "https://api.twitter.com/1.1/users/show.json"
+#urlSearchTweet = "https://api.twitter.com/1.1/search/tweets.json"
+#urlFriendTwitter = "https://api.twitter.com/1.1/friendships/create.json"
+#url = "https://api.twitter.com/1.1/users/suggestions/:slug.json"
+#url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+
 
 	def serachUserMessage(self) :
 		print "検索するワードを入れてください。"
